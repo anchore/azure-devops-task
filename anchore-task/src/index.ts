fcb53ccfe3cf878db2ed9e2be5419b00b547104c
James Petersen <jpetersenames@gmail.com>
2020-05-06 23:40:24 +0000

    Added basic functionality with inline scan
    Added the base case - stateless scan for a local image. Tested on
    Azure Pipeline and it ran everything but then received a 404 when
    trying to retrieve the results.
    
    ```
    	Policy Evaluation - ipserver:0.1.0
    -----------------------------------------------------------
    
    Error: could not find any evaluations for input images
    HTTP Code: 404
    Detail: {'error_codes': []}
    
    Cleaning up docker container: 12403-inline-anchore-engine
    ```
    
    Will debug more later.

--- anchore-task/src/index.ts
+++ anchore-task/src/index.ts
@@ -3,16 +3,23 @@ import tr = require('azure-pipelines-task-lib/toolrunner');
 
 import { InputFetch } from './InputFetch';
 
+
 async function run() {
     try {
         const fetch = new InputFetch();
-        console.log('URL',      fetch.url);
-        console.log('Username', fetch.username);
-        console.log('Password', fetch.password);
         console.log('Image',    fetch.image);
 
-        var toolpath = tl.which('docker');
-        console.log(toolpath);
+        var docker = tl.which('docker', true);
+        // var dockertool: tr.ToolRunner = tl.tool(docker).arg(['pull', 'docker.io/anchore/inline-scan:v0.7.1']);
+        // var code: number = await dockertool.exec();
+
+        var curl = tl.which('curl', true);
+        var curltool: tr.ToolRunner = tl.tool(curl).line('-s https://ci-tools.anchore.io/inline_scan-latest -o /tmp/inline_scan.sh');
+        var code: number = await curltool.exec();
+
+        var bash = tl.which('bash');
+        var inlinescan: tr.ToolRunner = tl.tool(bash).arg(['/tmp/inline_scan.sh', fetch.image]);
+        code = await inlinescan.exec();
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
